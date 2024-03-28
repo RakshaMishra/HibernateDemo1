f45b0dea1e9dade9ac18c4cd7fd63c1429a12036
Dell <Dell@DESKTOP-GIG0893>
2024-03-28 17:19:40 +0530

    hibernate demo 1

---
+++ src/main/java/com/kodnest/hibernetdemoproject/Hibernate/Demo1/App.java
@@ -0,0 +1,13 @@
+package com.kodnest.hibernetdemoproject.Hibernate.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
